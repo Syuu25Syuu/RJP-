24b60372248551f41d662c5e653d7387d4af549f
yt18304019 <44827902+yt18304019@users.noreply.github.com>
2020-02-25 17:46:47 +0900

    わすれてたの^^;

---
+++ 0225_New/changeProf.js
@@ -0,0 +1,10 @@
+$(function() {
+// inputタグから取得
+  $(".changeProfLike").on('click', function(){
+		 $('#likeform').submit();
+  });
+  $(".changeProfPage").on('click', function(){
+		 $('#showMoveForm').submit();
+  });
+
+});
